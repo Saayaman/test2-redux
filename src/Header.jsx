6e4707a20673d3186258b734953631ae227b80d1
Ayako <ayakosayama1987@gmail.com>
2019-10-09 10:06:16 -0700

    push

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import RefreshIcon from '@material-ui/icons/Refresh';
+// import RefreshIcon from '@material-ui/icons/Refresh';
 
 export default function ButtonAppBar() {
   return (
